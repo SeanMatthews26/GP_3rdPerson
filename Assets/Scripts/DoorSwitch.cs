f626c61f2ff4944c4fcae32401ede3c7ff80a3f0
Sean <Sean3.matthews@live.uwe.ac.uk>
2023-03-08 16:38:18 +0000

    door kinda working

--- Assets/Scripts/DoorSwitch.cs
+++ Assets/Scripts/DoorSwitch.cs
@@ -11,7 +11,13 @@ public class DoorSwitch : MonoBehaviour
     Vector3 playerSwitchPos;
     [SerializeField] float offset;
     Collider col;
-    
+    GameObject door;
+
+
+    void Awake()
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +26,12 @@ public class DoorSwitch : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         playerSwitchPos = transform.position + (transform.right * offset);
         col = GetComponent<Collider>();
+        door = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (playerControls.currentTarget == this.gameObject && playerControls.lockedOn)
-        {
-            Vector3 newPos = new Vector3(playerSwitchPos.x, player.transform.position.y, playerSwitchPos.z);
-            player.transform.position = Vector3.MoveTowards(player.transform.position, newPos, playerControls.maxSpeed);
-        }*/
-
         if(playerControls.interacting)
         {
             Collider[] hits = Physics.OverlapSphere(player.transform.position + player.transform.forward * playerControls.interactSphereOffset, playerControls.interactSphereRad);
@@ -39,11 +40,18 @@ public class DoorSwitch : MonoBehaviour
             {
                 if(hit == col)
                 {
-                    Vector3 newPos = new Vector3(playerSwitchPos.x, player.transform.position.y, playerSwitchPos.z);
-                    player.transform.position = Vector3.MoveTowards(player.transform.position, newPos, playerControls.maxSpeed);
-                    player.transform.forward = -transform.right;
+                    Switch();
                 }
             }
         }
+    }
+
+    void Switch()
+    {
+        Vector3 newPos = new Vector3(playerSwitchPos.x, player.transform.position.y, playerSwitchPos.z);
+        player.transform.position = Vector3.MoveTowards(player.transform.position, newPos, playerControls.maxSpeed);
+        player.transform.forward = -transform.right;
+
+        Destroy(door);
     }
 }
