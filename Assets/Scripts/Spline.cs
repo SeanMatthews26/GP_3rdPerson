d00abde9b708a23cd758c8504653eaa00e1b5c0b
Sean <Sean3.matthews@live.uwe.ac.uk>
2023-03-22 19:41:39 +0000

    update

--- Assets/Scripts/Spline.cs
+++ Assets/Scripts/Spline.cs
@@ -8,15 +8,18 @@ public class Spline : MonoBehaviour
     private int pointCount;
 
     //2.5D Test
-    [SerializeField] GameObject player;
+    private GameObject player;
     float currentClosestDistance = Mathf.Infinity;
     float tDistance = 0;
     Vector3 currentClosestPosition = Vector3.zero;
+    [SerializeField] bool findClosest;
 
     private Vector3 gizmosPosition;
 
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
         pointCount = transform.childCount;
 
         for (int i = 0; i < pointCount; i++)
@@ -37,31 +40,35 @@ public class Spline : MonoBehaviour
         }
 
         //2.5D Test
-        for (float t = 0; t <= 1; t += 0.04f)
+        if(findClosest)
         {
-            gizmosPosition = Mathf.Pow(1 - t, 3) * splinePoints[0].position + 3 * Mathf.Pow(1 - t, 2) * t * splinePoints[1].position + 3 * (1 - t) * Mathf.Pow(t, 2) * splinePoints[2].position + Mathf.Pow(t, 3) * splinePoints[3].position;
-
-            //Gizmos.DrawSphere(gizmosPosition, 0.25f);
-
-            //currentClosest = (gizmosPosition - player.transform.position).sqrMagnitude;
-
-            tDistance = (player.transform.position - gizmosPosition).sqrMagnitude;
-
-            //Check if this is closest
-            if(gizmosPosition == currentClosestPosition)
+            for (float t = 0; t <= 1; t += 0.04f)
             {
-                currentClosestDistance = tDistance;
-            }
+                gizmosPosition = Mathf.Pow(1 - t, 3) * splinePoints[0].position + 3 * Mathf.Pow(1 - t, 2) * t * splinePoints[1].position + 3 * (1 - t) * Mathf.Pow(t, 2) * splinePoints[2].position + Mathf.Pow(t, 3) * splinePoints[3].position;
 
-            if (currentClosestDistance > tDistance)
-            {
-                currentClosestDistance = tDistance;
-                currentClosestPosition = gizmosPosition;
-            }
+                //Gizmos.DrawSphere(gizmosPosition, 0.25f);
 
-            
+                //currentClosest = (gizmosPosition - player.transform.position).sqrMagnitude;
+
+                tDistance = (player.transform.position - gizmosPosition).sqrMagnitude;
+
+                //Check if this is closest
+                if (gizmosPosition == currentClosestPosition)
+                {
+                    currentClosestDistance = tDistance;
+                }
+
+                if (currentClosestDistance > tDistance)
+                {
+                    currentClosestDistance = tDistance;
+                    currentClosestPosition = gizmosPosition;
+                    Debug.Log(t);
+                }
+
+
+            }
+            Gizmos.DrawLine(currentClosestPosition, player.transform.position);
         }
-        Gizmos.DrawLine(currentClosestPosition, player.transform.position);
 
         Gizmos.DrawLine(new Vector3(splinePoints[0].position.x, splinePoints[0].position.y, splinePoints[0].position.z), new Vector3(splinePoints[1].position.x, splinePoints[1].position.y, splinePoints[1].position.z));
         Gizmos.DrawLine(new Vector3(splinePoints[2].position.x, splinePoints[2].position.y, splinePoints[2].position.z), new Vector3(splinePoints[3].position.x, splinePoints[3].position.y, splinePoints[3].position.z));
