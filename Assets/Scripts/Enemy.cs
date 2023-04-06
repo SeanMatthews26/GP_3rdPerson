b7b650c0699400d1fc999bb11c48de0780d71274
Sean <Sean3.matthews@live.uwe.ac.uk>
2023-04-06 16:09:02 +0100

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour
     [HideInInspector] public NavMeshAgent agent;
     [HideInInspector] public Transform playerTrans;
     public LayerMask groundLayer, playerLayer;
+    private Rigidbody rb;
 
     //Wander
     public Vector3 walkPoint;
@@ -17,15 +18,30 @@ public class Enemy : MonoBehaviour
     [SerializeField] float wanderRange;
     private Vector3 wanderDestination;
 
+    //Chase
+    [SerializeField] float enemySpeed;
+    public bool playerInSightRange;
+    public float sightRange;
+
     //Attacking
+    [SerializeField] float attackSpeed;
     public float timeBetweenAttacks;
     private bool attacked;
+    public bool playerInAttackRange;
+    public float attackRange;
+    [SerializeField] private float jumpForce;
+    
 
     //States
-    enum State { wander, chase, attack };
+    enum State 
+    {
+        WANDER,
+        CHASE,
+        ATTACK
+    };
+
     State currentState;
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
+
 
     //Combat
     [SerializeField] private float health;
@@ -33,47 +49,83 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentState = State.wander;
+        currentState = State.WANDER;
         startPos= transform.position;
 
         playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
+        agent.acceleration = 40;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(currentState.ToString());
+
+        agent.destination = playerTrans.position;
+
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
 
         //States
-        if(currentState == State.wander)
+        if(currentState == State.WANDER)
         {
             Wander();
         }
-        if (currentState == State.chase)
+        if (currentState == State.CHASE)
         {
             Chase();
+        }
+        if(currentState == State.ATTACK) 
+        {
+            Attack();
+        }
+    }
+
+    private void Wander()
+    {
+        agent.speed = 0;
+
+        if (playerInSightRange)
+        {
+            currentState = State.CHASE;
         }
     }
 
     void Chase()
     {
-        agent.destination = playerTrans.position;
+        agent.speed = enemySpeed;
+
+        if (playerInAttackRange)
+        {
+            currentState = State.ATTACK;
+        }
+    }
+
+    void Attack()
+    {
+        agent.speed = attackSpeed;
+        rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+        currentState = State.WANDER;
+    }
+
+
+    void ChangeStatesToWander()
+    {
+        currentState = State.WANDER;
     }
 
     private void OnDrawGizmos()
     {
+        //Sight Range
+        Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(transform.position, sightRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 
-    private void Wander()
-    {
-        if (playerInSightRange)
-        {
-            Debug.Log("PlayerSpotted");
-            currentState = State.chase;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
